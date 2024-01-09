743fc9e1b3993abdfe05de7b2126fec4dec09cb0
tajirri <clarencewagah@gmail.com>
2024-01-08 18:26:00 -0800

    Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    """Find all multiples of 2 in a list."""
+    m = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            m.append(True)
+        else:
+            m.append(False)
+
+    return (m)
