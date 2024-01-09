4929c270a645613477cab99c61e015cde7a87378
tajirri <clarencewagah@gmail.com>
2024-01-09 05:16:33 -0800

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(00, 100):
-    print("{}{}{}".format(i // 10, i % 10, ", "), end='')
-print("{}".format(99))
+        if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
